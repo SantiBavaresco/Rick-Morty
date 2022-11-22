0af699aa5076715fdd43a2834469ad17a6b39baf
Santiago Bavaresco <santiago.bavaresco@codere.com>
2022-11-22 09:53:17 -0300

    Estilos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,18 @@ export default function Card(props) {
    //console.log(props);
    return (
       <div className={styles.card}>
-         
-         <button className={styles.xbutton} onClick={props.onClick}> <img  src="https://i.pinimg.com/originals/78/a9/6d/78a96d96ffa60a4477d0f17e9bd55a36.png" alt={props.name} /></button>
-         
+        <div>
+         <button className={styles.xbutton} onClick={props.onClick}> 
+            <img  src="https://i.pinimg.com/originals/78/a9/6d/78a96d96ffa60a4477d0f17e9bd55a36.png" alt={props.name} 
+               // className={styles.xbutton.img}
+               />
+         </button>
+         </div> 
+         <img  src={props.image} alt={props.name}></img>
          <h2 className={styles.subtitle}>{props.name}</h2>
          <h2 className={styles.subtitle}>{props.species}</h2>
          <h2 className={styles.subtitle}>{props.gender}</h2>
-         <img  src={props.image} alt={props.name} />
+          
       </div>
    );
 }
