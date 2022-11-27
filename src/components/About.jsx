3ec32218bd33add87e619c58ff364ce8b7230c36
SantiBavaresco <103169408+SantiBavaresco@users.noreply.github.com>
2022-11-27 02:55:38 -0300

    routing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,19 @@
 import styles from '../modules/Bienvenido.module.css';
 import { useParams, useNavigate } from "react-router-dom";
 
-
-
 export default function About(){
     const navigate = useNavigate();
     return( 
     <div className={styles.about}>
+        <span > 
+            <h1>Hola soy Santi</h1>
+            <p>Esta es mi historia..</p>
+        </span>
+
         <div>
             <button onClick={()=>navigate("/home")} className={styles.button}
             style={{marginTop: "2%", marginBottom: "2%"}}>Volver</button>
         </div>
-      
-        <span > 
-        <h1>Hola soy Santi</h1>
-        <p>Esta es mi historia..</p>
-        </span>
     </div>
     );
 }
